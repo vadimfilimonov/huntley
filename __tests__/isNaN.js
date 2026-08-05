542e204e6bc07dfc737e6db65463a72003a31f35
Vadim Filimonov <filimonov@duck.com>
2026-08-05 23:07:20 +0300

    Fix isNaN test constructors
    Instantiate Date and Error values explicitly in the negative test cases.

--- __tests__/isNaN.js
+++ __tests__/isNaN.js
@@ -16,9 +16,9 @@ describe('isNaN', () => {
     expect(isNaN({ a: 1 })).toEqual(false);
     expect(isNaN(true)).toEqual(false);
     // eslint-disable-next-line prettier/prettier
-    expect(isNaN(new Date)).toEqual(false);
+    expect(isNaN(new Date())).toEqual(false);
     // eslint-disable-next-line prettier/prettier
-    expect(isNaN(new Error)).toEqual(false);
+    expect(isNaN(new Error())).toEqual(false);
     expect(isNaN(/x/)).toEqual(false);
   });
 });
